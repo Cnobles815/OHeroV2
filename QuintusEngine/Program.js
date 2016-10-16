ff5579c33ecaa7472cc92dda0b2fde9189ab7d37
cnobles <cnobles815@gmail.com>
2016-10-16 09:44:11 -0400

    Background testing.

--- QuintusEngine/Program.js
+++ QuintusEngine/Program.js
@@ -6,6 +6,7 @@ Q.load(["basic-background.png"], function(){
     var background = new Q.Sprite({ asset: "basic-background.png", x: Q.el.width/2, y: Q.el.height/2, type: Q.SPRITE_NONE});
     Q.gameLoop(function(dt){
         Q.clear();
+        background.render(Q.ctx);
     
     });
 });
