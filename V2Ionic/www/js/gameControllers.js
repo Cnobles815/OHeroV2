ab113cff397eea8a6bdd99c5d84c39bc4c80c05c
cnobles <cnobles815@gmail.com>
2016-10-18 17:58:22 -0400

    Possible change in direction.

--- V2Ionic/www/js/gameControllers.js
+++ V2Ionic/www/js/gameControllers.js
@@ -1,40 +1,44 @@
 angular.module('game.controllers', [])
 
 .controller('GameCtrl', function($scope) {
+
+var canvas = document.getElementById('gameSpace');
+var context = canvas.getContext('2d');
+
   $scope.runGame = function(){
     <script src="http://cdn.html5Quintus.com/v0.1.5/quintus-all.js"></script>
-    var Q = Quintus()
+    var Q = Quintus({development: true})
     .include("Sprites, Scenes")
-    .setup({maximize : true})
+    .setup(canvas)
 
-// Q.Sprite.extend("Enemy", {
-//     init: function(p) {
-//         this._super(p, {sheet: 'enemy', vx: 100, visibleOnly: true});
+Q.Sprite.extend("Enemy", {
+    init: function(p) {
+        this._super(p, {sheet: 'enemy', vx: 100, visibleOnly: true});
 
-//         this.add('2d, aiBounce');
-//         //TODO -- Need to add projectile collision (Firearm/Focus etc)
-//         //TODO -- Figure out how the hell shield/barrier are going to work. Stick sprite to Player?
-//         //TODO -- Refactor collision to reduce health rather than instantly kill.
-//         this.on("bump.left,bump.right,bump.bottom", function(collision){
-//             if(collision.obj.isA("Player")) {
-//                 Q.stageScene("endGame",1,{ label: "You Died"});
-//                 collision.obj.destroy();
-//             }
-//             if(collision.obj.isA("Projectile")) {
-//                 this.destroy();
-//                 collision.obj.p.vy = -300;
-//             }
-//         });
+        this.add('2d, aiBounce');
+        //TODO -- Need to add projectile collision (Firearm/Focus etc)
+        //TODO -- Figure out how the hell shield/barrier are going to work. Stick sprite to Player?
+        //TODO -- Refactor collision to reduce health rather than instantly kill.
+        this.on("bump.left,bump.right,bump.bottom", function(collision){
+            if(collision.obj.isA("Player")) {
+                Q.stageScene("endGame",1,{ label: "You Died"});
+                collision.obj.destroy();
+            }
+            if(collision.obj.isA("Projectile")) {
+                this.destroy();
+                collision.obj.p.vy = -300;
+            }
+        });
 
-//         this.on("bump.top",function(collision) {
-//             if(collision.obj.isA("Player")) {
-//                 this.destroy();
-//                 collision.obj.p.vy = -300;
-//             }
-//         });
+        this.on("bump.top",function(collision) {
+            if(collision.obj.isA("Player")) {
+                this.destroy();
+                collision.obj.p.vy = -300;
+            }
+        });
 
-//     }
-// });
+    }
+});
 
 Q.Sprite.extend("Player", {
     init:function(p) {
@@ -49,46 +53,46 @@ Q.Sprite.extend("Player", {
 });
 
 
-// Q.Sprite.extend("Projectile", {
-//     init:function(p) {
-//         this._super(p, {
-//             asset:"",
-//             type: Q.SPRITE_FRIENDLY,
-//             speed: 300
-//         })
-//     }
-// })
+Q.Sprite.extend("Projectile", {
+    init:function(p) {
+        this._super(p, {
+            asset:"",
+            type: Q.SPRITE_FRIENDLY,
+            speed: 300
+        })
+    }
+})
 
-// Q.Projectile.extend("Beam", {
-//     init:function(p) {
-//         this._super(p, {
-//             asset:"",
-//             type: Q.SPRITE_FRIENDLY
-//         })
-//     }
-// })
+Q.Projectile.extend("Beam", {
+    init:function(p) {
+        this._super(p, {
+            asset:"",
+            type: Q.SPRITE_FRIENDLY
+        })
+    }
+})
 
 
 //TODO -- NEVERMIND. Remember to spell function correctly FFR
-// Q.scene("level1",function(stage){
-//     Q.stageTMX("level1.tmx",stage);
-//     stage.add("viewport").follow(Q("Player").first());
-// });
+Q.scene("level1",function(stage){
+    Q.stageTMX("level1.tmx",stage);
+    stage.add("viewport").follow(Q("Player").first());
+});
 
-// Q.scene('endGame',function(stage){
-//     var container = stage.insert(new Q.UI.Container({
-//         x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
-//     }));
+Q.scene('endGame',function(stage){
+    var container = stage.insert(new Q.UI.Container({
+        x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+    }));
 
-//     var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
-//                                                     label: "Play Again"}))
-//     var label = container.insert(new Q.UI.Text({x:10, y: -10 - button.p.h,
-//                                                 label: stage.options.label }));
-//     button.on("click",function(){
-//         Q.clearStages();
-//         Q.stageScene('level1');
-//     })
-// })
+    var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
+                                                    label: "Play Again"}))
+    var label = container.insert(new Q.UI.Text({x:10, y: -10 - button.p.h,
+                                                label: stage.options.label }));
+    button.on("click",function(){
+        Q.clearStages();
+        Q.stageScene('level1');
+    })
+})
 
 Q.load(["/Images/basic-background.png"], function(){
     var background = new Q.Sprite({ asset: "/Images/basic-background.png", x: Q.el.width / 2, y: Q.el.height / 2, type: Q.SPRITE_NONE});
